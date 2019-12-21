baf043e4e847920146d638e65a304dbbde104c2f
Den <fentau.line@gmail.com>
2019-12-21 12:24:28 +0200

    initialize commit

--- 09.10.2019_M3_W11/JS_DZ_Modul_3_Week_11/task_1/app/main.js
+++ 09.10.2019_M3_W11/JS_DZ_Modul_3_Week_11/task_1/app/main.js
@@ -1,3 +1,20 @@
 const myLink = {
   inp: document.querySelector('#inp')
 }
+
+myLink.inp.addEventListener('input', (e) => {
+  let sumText = e.target.value,
+      lng = sumText.length;
+
+      if (!e.data){
+        return;
+    }else if (e.data.charCodeAt(0) > 64 && e.data.charCodeAt(0) < 91){
+        return;
+    }else if (e.data.charCodeAt(0) > 96 && e.data.charCodeAt(0) < 123){
+        return;
+    }else if (e.data.charCodeAt(0) > 1039 && e.data.charCodeAt(0) < 1103){
+        return;
+    }else if (e.data.charCodeAt(0) != 32){
+        e.target.value = sumText.slice(0, lng - 1);
+    }
+});
